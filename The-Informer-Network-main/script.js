90a38037ea535e6d791bf5811c40c0ebe7ab6088
Vanshikanainn <143733469+Vanshikanainn@users.noreply.github.com>
2025-07-03 14:46:22 +0530

    Update script.js

--- The-Informer-Network-main/script.js
+++ The-Informer-Network-main/script.js
@@ -4,7 +4,7 @@ const logo = document.getElementById('logo');
 const newsContainer1 = document.getElementById('top-news-container1');
 const newsContainer2 = document.getElementById('top-news-container2');
 const categoryContainer = document.getElementById('category-container');
-const apiKey = '25a39e1a5d554c3c888227e36b8d50dc';
+const apiKey = 'API KEY';
 const constUrl = `https://newsapi.org/v2/top-headlines?country=in&pageSize=10&apiKey=${apiKey}`;
 const inputKeyword = document.getElementById('inputKeyword');
 const submitKeyword = document.getElementById('submitKeyword');
